e87776157f2353fc076d60ec2ffae2c9bd18c122
Syed Muhammad Ahmed <m.ahmed@commerciotech.com>
2023-03-09 18:16:16 +0500

    desktop view created

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log("Welcome to the Electron Js World !");
+const path = require("path");
+const { app, BrowserWindow } = require("electron");
+
+createMainWindow = () => {
+  const mainWindow = new BrowserWindow({
+    title: "Image Resizer",
+    width: 700,
+    height: 800,
+  });
+
+  mainWindow.loadFile(path.join(__dirname, "./renderer/index.html"));
+};
+
+app.whenReady().then(() => {
+  createMainWindow();
+});
