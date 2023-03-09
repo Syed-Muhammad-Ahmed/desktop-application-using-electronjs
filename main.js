65a73e0d61ba82259c6be9742005b7a1c06e4b01
Syed Muhammad Ahmed <m.ahmed@commerciotech.com>
2023-03-09 18:46:44 +0500

    added some changes

--- main.js
+++ main.js
@@ -1,16 +1,33 @@
 const path = require("path");
 const { app, BrowserWindow } = require("electron");
 
+const isMac = process.platform === "darwin";
+
 createMainWindow = () => {
   const mainWindow = new BrowserWindow({
     title: "Image Resizer",
-    width: 700,
+    width: 800,
     height: 800,
   });
+
+  // open dev tools
+  mainWindow.webContents.openDevTools();
 
   mainWindow.loadFile(path.join(__dirname, "./renderer/index.html"));
 };
 
 app.whenReady().then(() => {
   createMainWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createMainWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (!isMac) {
+    app.quit();
+  }
 });
